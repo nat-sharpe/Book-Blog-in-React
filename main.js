298a44edcf10bf6fa5142fd55d8ea176e7bbca3d
Nat Sharpe <sharpe1890@gmail.com>
2018-09-10 13:56:30 -0400

    Dynamically creates list of books

--- main.js
+++ main.js
@@ -13,29 +13,16 @@ let books = [
     },
 ];
 
-// let bookList = [];
-
-// getBookList = books => {
-//     books.forEach(book => {
-//         bookList.push(h('li', { className: 'book' }, [`${book.title} by ${book.author}`]),`)
-//     })
-// };
-
-// getBookList(books);
-
 let h = React.createElement;
 
+let bookList = books.map(book =>
+        h('li', { className: 'book' }, [`${book.title} by ${book.author}`])
+    );
 
 let content = h('div', null, [
     h('h1', { className: 'big-text' }, ['Some cool books!']),
-    h('ul', { className: 'book-list' }, [
-        h('li', { className: 'book' }, [`${books[0].title} by ${books[0].author}`]),
-        h('li', { className: 'book' }, [`${books[1].title} by ${books[1].author}`]),
-        h('li', { className: 'books' }, [`${books[2].title} by ${books[2].author}`]),
-        h('li', { className: 'books' }, [`${books[3].title} by ${books[3].author}`]),
-    ]),
+    h('ul', { className: 'book-list' }, bookList),
     h('footer', null, ['Copyright 2018']),
 ]);
-
 
 ReactDOM.render(content, document.querySelector('.react-root'));
