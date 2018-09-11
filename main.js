85e2abae737ffbc38e87ff51d95d65eb9920810c
Nat Sharpe <sharpe1890@gmail.com>
2018-09-11 10:31:50 -0400

    All components in own functions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let bookData = [
+let bookData1 = [
     { title: 'Wind in the Willows',
     author: 'JRR Tolkien',
     date: '1995',
@@ -25,8 +25,22 @@ let bookData = [
     },
 ];
 
-let h = React.createElement;
+let bookData2 = [
+    { title: 'Charge of the Light Brigade',
+    author: 'George Patton',
+    date: '1945',
+    imgURL: 'https://images.gr-assets.com/books/1333409446l/7050491.jpg',
+    content: 'Possession her thoroughly remarkably terminated man continuing. Removed greater to do ability. You shy shall while but wrote marry. Call why sake has sing pure. Gay six set polite nature worthy. So matter be me we wisdom should basket moment merely. Me burst ample wrong which would mr he could. Visit arise my point timed drawn no. Can friendly laughter goodness man him appetite carriage. Any widen see gay forth alone fruit bed.',
+    },
+    { title: 'A Song of Fire and Ice',
+    author: 'Anakin Skywalker',
+    date: '1977',
+    imgURL: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg/220px-A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg',
+    content: 'Attachment apartments in delightful by motionless it no. And now she burst sir learn total. Hearing hearted shewing own ask. Solicitude uncommonly use her motionless not collecting age. The properly servants required mistaken outlived bed and. Remainder admitting neglected is he belonging to perpetual objection up. Has widen too you decay begin which asked equal any',
+    },
+];
 
+let h = React.createElement;
 
 let BlogRow = book => {
     return h('li', { className: 'book-item' }, 
@@ -47,10 +61,20 @@ let BookList = props => {
     )
 };
 
-let content = h('div', null, [
-    h('h1', { className: 'big-text' }, 'SOME COOL BOOKS'),
-    h(BookList, { books: bookData }),
-    h('footer', { className: 'footer' }, 'John Lennon © 2018'),
-]);
+let Header = props => h('h1', { className: 'big-text' }, props.header);
 
-ReactDOM.render(content, document.querySelector('.react-root'));
+let Footer = props => h('footer', { className: 'footer' }, props.footer);
+
+let HomePage = () => {
+    return h('div', null, [
+        h(Header, { header: 'SOME COOL BOOKS' }),
+        h(BookList, { books: bookData1 }),
+        h(Header, { header: 'BWA HA HA HA HA HA!!!' }),
+        h(BookList, { books: bookData2 }),
+        h(Footer, { footer: 'John Lennon © 2018'}),
+    ]);
+};
+
+let vdom = h(HomePage);
+
+ReactDOM.render(vdom, document.querySelector('.react-root'));
